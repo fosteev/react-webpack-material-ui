74b505f21daa7456a54232e0a3718a0146a38dc8
Andrey <fosteev3@gmail.com>
2020-01-27 12:38:04 +0300

    init React + webpack + babel + router 5

---
+++ src/Root.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+import './App.css';
+
+class Root extends Component {
+    render() {
+        return (<div>React + webpack 4 + react router 4 + babel 7</div>);
+    }
+
+}
+
+export default Root;
